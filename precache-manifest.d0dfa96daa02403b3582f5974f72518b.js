27bce920a1510aba5b61fdc7011e213418309f5b
Juan Garay <juangaray93@gmail.com>
2020-09-27 21:26:12 -0300

    Updates

---
+++ precache-manifest.d0dfa96daa02403b3582f5974f72518b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb2645542fe0e58b1c99d916ef9811d",
+    "url": "/sensum-web/index.html"
+  },
+  {
+    "revision": "29114791575040b73ba2",
+    "url": "/sensum-web/static/css/main.897b1b81.chunk.css"
+  },
+  {
+    "revision": "945ad2c21ad2a32ebe95",
+    "url": "/sensum-web/static/js/2.690a32e9.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/sensum-web/static/js/2.690a32e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29114791575040b73ba2",
+    "url": "/sensum-web/static/js/main.5ae8cdab.chunk.js"
+  },
+  {
+    "revision": "e2771a4853f8ec562753",
+    "url": "/sensum-web/static/js/runtime-main.d8d0830b.js"
+  },
+  {
+    "revision": "42066740fd97fc83cd97358497219cd1",
+    "url": "/sensum-web/static/media/close.42066740.svg"
+  },
+  {
+    "revision": "86b89acd6e2b3eee1e44f806bcf04215",
+    "url": "/sensum-web/static/media/pulse.86b89acd.svg"
+  }
+]);
